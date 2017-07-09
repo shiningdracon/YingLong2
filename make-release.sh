8bc02141bb2f393eb53683c43beee3740c8a3908
shiningdracon <shiningdracon@gmail.com>
2017-07-09 13:25:54 -0700

    Update make-release script

--- make-release.sh
+++ make-release.sh
@@ -2,7 +2,7 @@
 rm -rf release.tar.bz2 release
 mkdir release
 cp .build/release/*.so release/
-cp .build/release/WebComicTranslatePlatform release/
+cp .build/release/YingLong2 release/
 mkdir -p release/views
 cp -r views/*.mustache release/views/
 
