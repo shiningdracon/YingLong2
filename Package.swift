2f560e11980b2586844b76b22cb4cf6da66ec5dd
shiningdracon <shiningdracon@gmail.com>
2017-09-16 02:32:49 -0700

    Update package dependence.

--- Package.swift
+++ Package.swift
@@ -8,9 +8,9 @@ let package = Package(
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mysql.git", majorVersion: 2),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
-		.Package(url: "https://github.com/shiningdracon/SwiftGD.git", majorVersion: 1),
-		.Package(url: "https://github.com/shiningdracon/OpenCC-swift.git", majorVersion: 1),
-		.Package(url: "https://github.com/shiningdracon/BBCode-Swift.git", majorVersion: 0),
+		.Package(url: "https://github.com/shiningdracon/SwiftGD.git", majorVersion: 1, minor: 4),
+		.Package(url: "https://github.com/shiningdracon/OpenCC-swift.git", majorVersion: 1, minor: 0),
+		.Package(url: "https://github.com/shiningdracon/BBCode-Swift.git", majorVersion: 0, minor: 4),
 		.Package(url: "https://github.com/shiningdracon/ImageUploader.git", majorVersion: 1, minor: 1),
 		.Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0),
 	]
